cc61259d3a209b084db1cb5849017573cb1e606c
Aaron Isakov <aaronisakov@gmail.com>
2021-10-22 12:18:05 -0400

    got the endpoint to work

--- server.js
+++ server.js
@@ -1,16 +1,37 @@
-import { datascraper } from "./webscraper/dataScraperAPI.mjs"
+// import webscraper from "webscraper/dataScraperAPI.js"
 
-const express = require('express');
+// const express = require('express');
+import express from "express";
 
 const app = express();
 const port = '8000';
-const message = 'nice';
-const webscraper = datascraper()
+const message = 'Welcome to the beginning of the Surf Reporter API!';
+let currWindInfo =  {  
+  'Wind Direction': '  W ( 270 deg true )',
+  'Wind Speed': ' 11.7 kts',
+}
+
+let currSwellInfo = {
+    'Significant Wave Height (WVHT):': '  4.3 ft',
+    'Swell Height (SwH):': '  3.0 ft',
+    'Swell Period (SwP):': '  7.1 sec ',
+    'Swell Direction (SwD):': '  S',
+    'Wind Wave Height (WWH):': '  3.3 ft',
+    'Wind Wave Period (WWP):': '  6.2 sec ',
+    'Wind Wave Direction (WWD):': '  S',
+    'Wave Steepness (STEEPNESS):': '     STEEP',
+    'Average Wave Period (APD):': '  5.1 sec'
+  }
 
 app.get('/', (req, res) => {
       // console.log('User made a get request to "/"', req);
-      res.send(webscraper);
+      console.log("Successfully made a get request!");
+      res.send(message)
   })
+
+app.get('/reports', (req, res) => {
+  return res.json({currWindInfo, currSwellInfo});
+})
 app.listen(port, () => {
       console.log(`Example app listening at http://localhost:${port}`);
   })
