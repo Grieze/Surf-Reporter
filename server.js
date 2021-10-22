06304dcbf2b0531b79dbf4e3dad6bf331fbe4ae7
Aaron Isakov <aaronisakov@gmail.com>
2021-10-21 23:01:42 -0400

    working on endpoint and exporting dataScrapperAPI into server.js

--- server.js
+++ server.js
@@ -1,2 +1,16 @@
-// import webscraper from "webscraper/dataScraperAPI.js"
+import { datascraper } from "./webscraper/dataScraperAPI.mjs"
 
+const express = require('express');
+
+const app = express();
+const port = '8000';
+const message = 'nice';
+const webscraper = datascraper()
+
+app.get('/', (req, res) => {
+      // console.log('User made a get request to "/"', req);
+      res.send(webscraper);
+  })
+app.listen(port, () => {
+      console.log(`Example app listening at http://localhost:${port}`);
+  })
