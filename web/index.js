0af5c8a4ccc5bc2d8d792ed6735e62683a5f996a
Aaron Isakov <aaronisakov@gmail.com>
2021-10-31 23:02:25 -0400

    made changes to webscraper to reduce number of histWindData and modified data structure of scraped data. Front end now shows current wind and swell info. Historical info is a work in progress. Very very basic for the time being, have not touched react yet.

--- web/index.js
+++ web/index.js
@@ -1,18 +1,57 @@
 window.addEventListener('DOMContentLoaded', (event) => {
-    let root = document.getElementById("root");
-    let info = document.getElementById("info");
-    let wind = document.getElementById("wind");
-    let wave = document.getElementById("wave");
+    let currWind = document.getElementById("currWind");
+    let currWave = document.getElementById("currWave");
+    let histWind = document.getElementById("histWind");
+    let histWave = document.getElementById("histWave");
     
     // function returns a promise
     async function getData() {
         const response = await fetch('http://localhost:8000/reports');
         const data = await response.json();
         console.log('data', data) // data is a JSON object
-        root.textContent = "CurrWindInfo: " + JSON.stringify(data.currWindInfo['windDirection']);
-        let WVHT = document.createElement("div");
-        WVHT.textContent = "Significant Wave Height: " + JSON.stringify(data.currSwellInfo["waveHeight"]);
-        info.appendChild(WVHT);
+
+        // adding all of the data to the front end
+
+        // Current Wind Data
+        let windDirection = document.createElement("div");
+        windDirection.textContent = "Wind Direction: " + JSON.stringify(data.currWindInfo["windDirection"]);
+        let windSpeed = document.createElement("div");
+        windSpeed.textContent = "Wind Speed: " + JSON.stringify(data.currWindInfo["windSpeed"]);
+
+        currWind.appendChild(windDirection);
+        currWind.appendChild(windSpeed);
+        
+        // Current Wave Data
+        let waveHeight = document.createElement("div");
+        waveHeight.textContent = "Significant Wave Height: " + JSON.stringify(data.currSwellInfo["waveHeight"]);
+        let swellHeight = document.createElement("div");
+        swellHeight.textContent = "Swell Height: " + JSON.stringify(data.currSwellInfo["swellHeight"]);
+        let swellPeriod = document.createElement("div");
+        swellPeriod.textContent = "Swell Period: " + JSON.stringify(data.currSwellInfo["swellPeriod"]);
+        let swellDirection = document.createElement("div");
+        swellDirection.textContent = "Swell Direction: " + JSON.stringify(data.currSwellInfo["swellDirection"]);
+        let windWaveHeight = document.createElement("div");
+        windWaveHeight.textContent = "Wind Wave Height: " + JSON.stringify(data.currSwellInfo["windWaveHeight"]);
+        let windWavePeriod = document.createElement("div");
+        windWavePeriod.textContent = "Wind Wave Period: " + JSON.stringify(data.currSwellInfo["windWavePeriod"]);
+        let windWaveDirection = document.createElement("div");
+        windWaveDirection.textContent = "Wind Wave Direction: " + JSON.stringify(data.currSwellInfo["windWaveDirection"]);
+        let steepness = document.createElement("div");
+        steepness.textContent = "Steepness: " + JSON.stringify(data.currSwellInfo["steepness"]);
+        let averageWavePeriod = document.createElement("div");
+        averageWavePeriod.textContent = "Average Wave Period: " + JSON.stringify(data.currSwellInfo["averageWavePeriod"]);
+        
+        // appending all the data
+        currWave.appendChild(waveHeight);
+        currWave.appendChild(swellHeight);
+        currWave.appendChild(swellPeriod);
+        currWave.appendChild(swellDirection);
+        currWave.appendChild(windWaveHeight);
+        currWave.appendChild(windWavePeriod);
+        currWave.appendChild(windWaveDirection);
+        currWave.appendChild(steepness);
+        currWave.appendChild(averageWavePeriod);
+
         // inside function data is not a promise
         // we can do async stuff
         return data;
--- webscraper/dataScraperAPI.js
+++ webscraper/dataScraperAPI.js
@@ -19,49 +19,52 @@ function scrape() {
 
     // Scraping info for currSwellInfo
     currSwellInfo['waveHeight']      = $('table:nth-child(6) > tbody:nth-child(2) > tr:nth-child(2) > td:nth-child(3)').text().trim();
-    currSwellInfo['SwH']       = $('table:nth-child(6) > tbody:nth-child(2) > tr:nth-child(3) > td:nth-child(3)').text().trim();
-    currSwellInfo['SwP']       = $('table:nth-child(6) > tbody:nth-child(2) > tr:nth-child(4) > td:nth-child(3)').text().trim();
-    currSwellInfo['SwD']       = $('table:nth-child(6) > tbody:nth-child(2) > tr:nth-child(5) > td:nth-child(3)').text().trim();
-    currSwellInfo['WWH']       = $('table:nth-child(6) > tbody:nth-child(2) > tr:nth-child(6) > td:nth-child(3)').text().trim();
-    currSwellInfo['WWP']       = $('table:nth-child(6) > tbody:nth-child(2) > tr:nth-child(7) > td:nth-child(3)').text().trim();
-    currSwellInfo['WWD']       = $('table:nth-child(6) > tbody:nth-child(2) > tr:nth-child(8) > td:nth-child(3)').text().trim();
-    currSwellInfo['STEEPNESS'] = $('table:nth-child(6) > tbody:nth-child(2) > tr:nth-child(9) > td:nth-child(3)').text().trim();
-    currSwellInfo['APD']       = $('table:nth-child(6) > tbody:nth-child(2) > tr:nth-child(10) > td:nth-child(3)').text().trim();
-
-    // let currSwellTbodyLen = $("#data > table:nth-child(6) > tbody:nth-child(2)").children('tr').length;
-    // for (let i = 2; i < currSwellTbodyLen + 1; i++) {
-    //   currSwellInfo[$("table:nth-child(6) > tbody:nth-child(2) > tr:nth-child(" + i + ") > td:nth-child(2)").text()] =
-    //   $("table:nth-child(6) > tbody:nth-child(2) > tr:nth-child(" + i + ") > td:nth-child(3)").text().trim();
-    // }
+    currSwellInfo['swellHeight']       = $('table:nth-child(6) > tbody:nth-child(2) > tr:nth-child(3) > td:nth-child(3)').text().trim();
+    currSwellInfo['swellPeriod']       = $('table:nth-child(6) > tbody:nth-child(2) > tr:nth-child(4) > td:nth-child(3)').text().trim();
+    currSwellInfo['swellDirection']       = $('table:nth-child(6) > tbody:nth-child(2) > tr:nth-child(5) > td:nth-child(3)').text().trim();
+    currSwellInfo['windWaveHeight']       = $('table:nth-child(6) > tbody:nth-child(2) > tr:nth-child(6) > td:nth-child(3)').text().trim();
+    currSwellInfo['windWavePeriod']       = $('table:nth-child(6) > tbody:nth-child(2) > tr:nth-child(7) > td:nth-child(3)').text().trim();
+    currSwellInfo['windWaveDirection']       = $('table:nth-child(6) > tbody:nth-child(2) > tr:nth-child(8) > td:nth-child(3)').text().trim();
+    currSwellInfo['steepness'] = $('table:nth-child(6) > tbody:nth-child(2) > tr:nth-child(9) > td:nth-child(3)').text().trim();
+    currSwellInfo['averageWavePeriod']       = $('table:nth-child(6) > tbody:nth-child(2) > tr:nth-child(10) > td:nth-child(3)').text().trim();
 
     // Scraping info for histWindInfo
     let histWindTbodyLen = $("table.dataTable:nth-child(4) > tbody:nth-child(2)").children('tr').length;
+    let j = 0;
+    let regex = new RegExp(":40");
     for (let i = 3; i < histWindTbodyLen + 1; i++) {
-      let month = $("table.dataTable:nth-child(4) > tbody:nth-child(2) > tr:nth-child(" + i + ") > td:nth-child(1)").text() + "_";
-      let day   = $("table.dataTable:nth-child(4) > tbody:nth-child(2) > tr:nth-child(" + i + ") > td:nth-child(2)").text() + "_";
-      let time  = $("table.dataTable:nth-child(4) > tbody:nth-child(2) > tr:nth-child(" + i + ") > td:nth-child(3)").text();
-      histWindInfo[month + day + time] = {
-        "Wind Direction" : $('table.dataTable:nth-child(4) > tbody:nth-child(2) > tr:nth-child(' + i + ') > td:nth-child(4)').text().trim(),
-        "Wind Speed"     : $('table.dataTable:nth-child(4) > tbody:nth-child(2) > tr:nth-child(' + i + ') > td:nth-child(5)').text().trim(),
+      // only interested in times with :40 to sync up with histSwellInfo
+      if (regex.test($("table.dataTable:nth-child(4) > tbody:nth-child(2) > tr:nth-child(" + i + ") > td:nth-child(3)").text().trim())) {
+        histWindInfo[j] = {
+          'month'         : $("table.dataTable:nth-child(4) > tbody:nth-child(2) > tr:nth-child(" + i + ") > td:nth-child(1)").text().trim(),
+          'day'           : $("table.dataTable:nth-child(4) > tbody:nth-child(2) > tr:nth-child(" + i + ") > td:nth-child(2)").text().trim(),
+          'time'          : $("table.dataTable:nth-child(4) > tbody:nth-child(2) > tr:nth-child(" + i + ") > td:nth-child(3)").text().trim(),
+          "windDirection" : $('table.dataTable:nth-child(4) > tbody:nth-child(2) > tr:nth-child(' + i + ') > td:nth-child(4)').text().trim(),
+          "windSpeed"     : $('table.dataTable:nth-child(4) > tbody:nth-child(2) > tr:nth-child(' + i + ') > td:nth-child(5)').text().trim(),
+          }
+          j++;
         }
+      else 
+        // do nothing
+        continue;
       }
 
       // Scraping info for histSwellInfo
       let histSwellTbodyLen = $("table.dataTable:nth-child(7) > tbody:nth-child(2)").children('tr').length;
       for (let i = 3; i < histSwellTbodyLen + 1; i++) {
-        let month = $("table.dataTable:nth-child(7) > tbody:nth-child(2) > tr:nth-child(" + i + ") > td:nth-child(1)").text() + "_";
-        let day   = $("table.dataTable:nth-child(7) > tbody:nth-child(2) > tr:nth-child(" + i + ") > td:nth-child(2)").text() + "_";
-        let time  = $("table.dataTable:nth-child(7) > tbody:nth-child(2) > tr:nth-child(" + i + ") > td:nth-child(3)").text().trim();
-        histSwellInfo[month + day + time] = {
-            'WVHT'     : $('table.dataTable:nth-child(7) > tbody:nth-child(2) > tr:nth-child(' + i + ') > td:nth-child(4)').text().trim(),
-            'SwH'      : $('table.dataTable:nth-child(7) > tbody:nth-child(2) > tr:nth-child(' + i + ') > td:nth-child(5)').text().trim(),
-            'SwP'      : $('table.dataTable:nth-child(7) > tbody:nth-child(2) > tr:nth-child(' + i + ') > td:nth-child(6)').text().trim(),
-            'SwD'      : $('table.dataTable:nth-child(7) > tbody:nth-child(2) > tr:nth-child(' + i + ') > td:nth-child(7)').text().trim(),
-            'WWH'      : $('table.dataTable:nth-child(7) > tbody:nth-child(2) > tr:nth-child(' + i + ') > td:nth-child(8)').text().trim(),
-            'WWP'      : $('table.dataTable:nth-child(7) > tbody:nth-child(2) > tr:nth-child(' + i + ') > td:nth-child(9)').text().trim(),
-            'WWD'      : $('table.dataTable:nth-child(7) > tbody:nth-child(2) > tr:nth-child(' + i + ') > td:nth-child(10)').text().trim(),
-            'STEEPNESS': $('table.dataTable:nth-child(7) > tbody:nth-child(2) > tr:nth-child(' + i + ') > td:nth-child(11)').text().trim(),
-            'APD'      : $('table.dataTable:nth-child(7) > tbody:nth-child(2) > tr:nth-child(' + i + ') > td:nth-child(12)').text().trim(),
+        histSwellInfo[i-3] = {
+          'month'            : $("table.dataTable:nth-child(7) > tbody:nth-child(2) > tr:nth-child(" + i + ") > td:nth-child(1)").text().trim(),
+          'day'              : $("table.dataTable:nth-child(7) > tbody:nth-child(2) > tr:nth-child(" + i + ") > td:nth-child(2)").text().trim(),
+          'time'             : $("table.dataTable:nth-child(7) > tbody:nth-child(2) > tr:nth-child(" + i + ") > td:nth-child(3)").text().trim(),
+          'waveHeight'       : $('table.dataTable:nth-child(7) > tbody:nth-child(2) > tr:nth-child(' + i + ') > td:nth-child(4)').text().trim(),
+          'swellHeight'      : $('table.dataTable:nth-child(7) > tbody:nth-child(2) > tr:nth-child(' + i + ') > td:nth-child(5)').text().trim(),
+          'swellPeriod'      : $('table.dataTable:nth-child(7) > tbody:nth-child(2) > tr:nth-child(' + i + ') > td:nth-child(6)').text().trim(),
+          'swellDirection'   : $('table.dataTable:nth-child(7) > tbody:nth-child(2) > tr:nth-child(' + i + ') > td:nth-child(7)').text().trim(),
+          'windWaveHeight'   : $('table.dataTable:nth-child(7) > tbody:nth-child(2) > tr:nth-child(' + i + ') > td:nth-child(8)').text().trim(),
+          'windWavePeriod'   : $('table.dataTable:nth-child(7) > tbody:nth-child(2) > tr:nth-child(' + i + ') > td:nth-child(9)').text().trim(),
+          'windWaveDirection': $('table.dataTable:nth-child(7) > tbody:nth-child(2) > tr:nth-child(' + i + ') > td:nth-child(10)').text().trim(),
+          'steepness'        : $('table.dataTable:nth-child(7) > tbody:nth-child(2) > tr:nth-child(' + i + ') > td:nth-child(11)').text().trim(),
+          'averageWavePeriod': $('table.dataTable:nth-child(7) > tbody:nth-child(2) > tr:nth-child(' + i + ') > td:nth-child(12)').text().trim(),
         }
       }
       return { currWindInfo, currSwellInfo, histWindInfo, histSwellInfo };
@@ -71,7 +74,3 @@ function scrape() {
 }
 
 module.exports = scrape;
-// scrape();
-// console.log(info);
-
-// AsyncCall().then(res=>res.json()).then(data => doSomethingWithDataHere_itsnotaproniseanymorehere)
