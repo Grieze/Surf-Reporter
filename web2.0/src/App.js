73d9048b0fde0fd77378b78b930b7f3e26412ebb
Aaron Isakov <aaronisakov@gmail.com>
2021-12-14 15:12:44 -0500

    Created web2.0 directory which is a single page react app.

---
+++ web2.0/src/App.js
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Welcome to Surf Reporter</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
